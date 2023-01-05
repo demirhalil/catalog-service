8bcc50552a18fc9cec68abfa61367d40e9f5320f
Halil Demir <halil.demir@eu.panasonic.com>
2023-01-05 22:08:06 +0100

    implement book domain

---
+++ src/main/java/com/catalog/domain/BookAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.catalog.domain;
+
+public class BookAlreadyExistsException extends RuntimeException {
+    public BookAlreadyExistsException(String isbn) {
+        super("A book with ISBN " + isbn + " already exists.");
+    }
+}
---
+++ src/main/java/com/catalog/domain/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.catalog.domain;
+
+public class BookNotFoundException extends RuntimeException{
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+}
